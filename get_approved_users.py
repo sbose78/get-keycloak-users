782a9a3c4dde9f0c3082c942c054f46ab1869fea
Shoubhik Bose <sbose78@gmail.com>
2017-06-13 16:55:33 +0530

    first commit

---
+++ get_approved_users.py
@@ -0,0 +1,32 @@
+import os
+import json
+import requests
+
+SSO_SERVER = "https://" + os.environ['SSO_SERVER'] # sso.osio.org
+SSO_TOKEN = os.environ['SSO_ADMIN_API_TOKEN'] # without the 'bearer '
+APPROVED_USERS_FILE = "approved_users.json"
+
+SSO_URL = SSO_SERVER + "/auth/admin/realms/fabric8/users?max=1000&search=redhat.com"
+# A max value of 1000 is fine now, there are about 700 approved users now.
+# unfortunately there is no api which returns a count :(
+
+TOKEN_HEADER = "bearer " + SSO_TOKEN
+
+APPROVED_USERS = []
+
+print "Fetching data from keycloak, this will take a while....."
+RESPONSE = requests.get(SSO_URL, headers={'Authorization': TOKEN_HEADER})
+print "Server returned response code %d"%(RESPONSE.status_code)
+
+if RESPONSE.status_code == 200:
+    print "Creating list of approved users.."
+    JSON_USERS = json.loads(RESPONSE.text)
+    for user in JSON_USERS:
+        if 'attributes' in user.keys()  and 'approved' in user['attributes'].keys():
+            APPROVED_USERS.append(user)
+
+    print "Total approved users : %d"%(len(APPROVED_USERS))
+    with open(APPROVED_USERS_FILE, 'w') as outfile:
+        json.dump(APPROVED_USERS, outfile, sort_keys=True, indent=4, encoding='utf-8')
+    
+    print "Approved users' info written to %s "%(APPROVED_USERS_FILE)
